bf5bc031eb91a90c6854bd94c362f0c16a9153ab
gibsondan <daniel@elementl.com>
2020-08-12 11:14:04 -0400

    grace period

--- python_modules/dagster/dagster/grpc/server.py
+++ python_modules/dagster/dagster/grpc/server.py
@@ -686,7 +686,7 @@ def server_termination_target(termination_event, server):
     while not termination_event.is_set():
         time.sleep(0.1)
     # We could make this grace period configurable if we set it in the ShutdownServer handler
-    server.stop(grace=None)
+    server.stop(grace=5)
 
 
 class DagsterGrpcServer(object):
